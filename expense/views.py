c7947b9b1dcac209d52386bddc8c8fffeef69ad8
YenHsinWu <rex19960408@gmail.com>
2025-11-21 00:12:03 +0800

    implement form reading and save data in view

--- expense/views.py
+++ expense/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .forms import ExpenseForm
+from .models import Expense
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+
+    expenses = Expense.objects.all()
     expense_form = ExpenseForm()
     context = {
+        "expenses": expenses,
         "expense_form": expense_form,
     }
     return render(request, "expense/index.html", context)
