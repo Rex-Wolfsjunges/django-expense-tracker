95c81040996398cf076d4ea8bd5de416fdf8eb5e
YenHsinWu <rex19960408@gmail.com>
2025-11-21 00:06:16 +0800

    write the template of index page

--- expense/views.py
+++ expense/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .forms import ExpenseForm
 
 # Create your views here.
 def index(request):
-    return render(request, "expense/index.html")
+    expense_form = ExpenseForm()
+    context = {
+        "expense_form": expense_form,
+    }
+    return render(request, "expense/index.html", context)
